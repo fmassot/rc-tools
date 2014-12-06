67834cc4be1f5a0d948e0efc4a30d8b0d2de943e
François Massot <francois.massot@gmail.com>
2014-12-06 19:05:03 +0100

    clean

--- nosdeputes/tasks.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-
-
-def download_liasse_order(id_loi, id_dossier, id_examen):
-    url = "http://www2.assemblee-nationale.fr/recherche/query_amendements?typeDocument=amendement&idExamen=%s&idDossierLegislatif=%s&numAmend=&idAuteur=&idArticle=&idAlinea=&sort=&dateDebut=&dateFin=&periodeParlementaire=&texteRecherche=&rows=2500&format=html&tri=ordreTexteasc&typeRes=liste&start=" % (id_examen, id_dossier)
