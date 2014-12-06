85882901207e68125689d74feb108f1a9bb15f3c
François Massot <francois.massot@gmail.com>
2014-12-06 23:08:59 +0100

    add command to parse question

--- assemble_nationale/manage.py
+++ assemble_nationale/manage.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import click
+import requests
 import sys
+import json
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).absolute().parents[1]))
 
 from assemble_nationale.service import AmendementService
+from assemble_nationale.parsing.question_parser import parse_question
 
 
 @click.group()
@@ -20,6 +23,14 @@ def show_amendements_order(id_dossier, id_examen):
     results = AmendementService().get_amendement_order(id_dossier, id_examen)
     print 'Nombre d\'amendements   : %s' % (len(results),)
     print 'Ordre des ammendements : %s' % (','.join(results),)
+
+
+@cli.command()
+@click.argument('url')
+def parse_question_from_url(url):
+    question_html = requests.get(url).content
+    parsed_data = parse_question(url, question_html)
+    print json.dumps(parsed_data, indent=4)
 
 
 if __name__ == '__main__':
