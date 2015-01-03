46e82e562ebfcfa9317fa110c2a7008d05035b36
François Massot <francois.massot@gmail.com>
2015-01-03 01:05:18 +0100

    Fix default config

--- settings.py
+++ settings.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
 import ConfigParser
 
 db_config = ConfigParser.ConfigParser()
-db_config.read('db.cfg')
+
+if os.path.isfile('db.cfg'):
+    db_config.read('db.cfg')
+else:
+    db_config.read('db_config.sample')
+
