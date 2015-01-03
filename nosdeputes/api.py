7f4590f98c1406fdb08fa347ab71e9f5d5f459fc
François Massot <francois.massot@gmail.com>
2015-01-03 17:48:43 +0100

    Add synthese endpoint in nosdeputes api

--- nosdeputes/api.py
+++ nosdeputes/api.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
 
 ### API OF NOSDEPUTES.FR ###
+
+import requests
+
+
+class NDApi(object):
+    def __init__(self):
+        self.base_url = 'http://www.nosdeputes.fr'
+
+    def synthese(self, month=None, format='json'):
+        """
+        month format: YYYYMM
+        """
+        if month is None:
+            month = 'data'
+        url = self.base_url + '/synthese/' + month + '/' + format
+
+        if format == 'json':
+            return requests.get(url).json()
+        else:
+            return requests.get(url).content
